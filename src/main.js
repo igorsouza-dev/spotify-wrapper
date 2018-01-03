cebfcc668e36843ac548e78838e2c5ed1711f3e8
igormsouza <havok1305@gmail.com>
2018-01-03 18:27:57 -0300

    Adding tests

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchAlbums = () => {};
+export const searchTracks = () => {};
+export const searchArtists = () => {};
+export const searchPlaylists = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,3 +1,27 @@
-describe('Main', function(){
+import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from '../src/main';
 
+describe('Spotify Wrapper', () => {
+  describe('smoke tests', () => {
+    it('should exists the search method', () => {
+      expect(search).to.exist;
+    });
+    it('should exists the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+    it('should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+    it('should exists the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+  describe('Generic Search', () => {
+    it('should call the fetch function', () => {
+      const artists = search();
+    });
+  });
 });
